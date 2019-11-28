0efe915507c616339b977f2978a3c74ff385640b
DBTee <du57y@live.ca>
2019-11-27 16:31:46 -0800

    edit page stuff

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,8 @@ class WelcomeController < ApplicationController
     before_action :authenticate_user!
     
     def index
+        if current_user.rsvp < 1
+            redirect_to edit_user_path(current_user)
+        end
     end
 end
